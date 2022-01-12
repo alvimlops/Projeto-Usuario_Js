2fd103c4bde1e7680a3a10257cb050b1d2f30a5b
alvaro Rodrigues <alvim.lopez@gmail.com>
2022-01-12 11:18:54 -0300

    trabalhando com eventos

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert ("hello word!")
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
+
+
+
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    fields.forEach(function(field, index){
+        if (fields.name == "gender"){
+            if (fields.checked){
+                user[fields.name] = fields.value;
+            }
+        } else {
+            user[field.name] = field.value;
+        }
+    });
+    
+    console.log(user);
+});
