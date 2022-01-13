88267deef03ebbc435bb5b41eff9dc67494f8841
alvaro Rodrigues <alvim.lopez@gmail.com>
2022-01-13 19:08:54 -0300

    adicionando a class User

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ document.getElementById("form-user-create").addEventListener("submit", function(
             user[field.name] = field.value;
         }
     });
+
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin );
     
     addLine(User);
 });
